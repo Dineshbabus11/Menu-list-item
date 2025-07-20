ae6ed16dc067a33382321969c2af57d4b51179cb
Dinesh Babu S <msgtodineshbabus@gmail.com>
2025-07-20 20:32:28 +0530

    All files updated

---
+++ android/app/src/main/kotlin/com/example/menu_item_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.menu_item_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          title: Text("Menu Item",
+          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow:[
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(0, 5)
+                  )
+                ]
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Dosa",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  Text("Annapoorna EST 1918"),
+                  Text("Rs.89.09",style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    )
+  );
+}
